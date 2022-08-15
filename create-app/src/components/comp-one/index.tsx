0d399058fcfb01033e1ddc44ff96b8142b935f5a
thetime50 <1004763777@qq.com>
2022-08-16 02:17:52 +0800

    anno: prettier test

--- create-app/src/components/comp-one/index.tsx
+++ create-app/src/components/comp-one/index.tsx
@@ -3,9 +3,6 @@ import style from "./index.module.scss";
 console.log("style", style);
 
 export default function ComputedOne() {
-
-
-  
   return (
     <>
       <p className={style["comp-one"]}>Hi, I'm computed one.</p>
