4dc75ed5cd0c50ca42ee528da09597109ddeffc5
thetime50 <1004763777@qq.com>
2022-08-16 00:36:48 +0800

    feat: baseUrl sass

---
+++ create-app/src/components/comp-one/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from './index.module.scss';
+console.log('style', style)
+
+export default function ComputedOne() {
+    return (
+        <>
+            <p className={style['comp-one']}>Hi, I'm computed one.</p>
+        </>);
+}
