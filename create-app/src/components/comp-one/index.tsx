9ad743736fcfef61dcf00b17ebd639c5618e8d35
thetime50 <1004763777@qq.com>
2022-08-16 12:30:27 +0800

    feat: 1. CSS Reset 2. classnames 3. craco-plugin-scoped-css

--- create-app/src/components/comp-one/index.tsx
+++ create-app/src/components/comp-one/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import style from "./index.module.scss";
+import "./index.scoped.scss";
+import classnames from "classnames";
 console.log("style", style);
 
 export default function ComputedOne() {
   return (
     <>
-      <p className={style["comp-one"]}>Hi, I'm computed one.</p>
+      <p className={classnames([style["comp-one"]], { "comp-one": true })}>
+        Hi, I'm computed one.
+      </p>
+      <p className="comp-one">Hi, I'm computed one. p2</p>
     </>
   );
 }
