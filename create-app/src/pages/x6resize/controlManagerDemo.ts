fdd98f7819330e3a32fae73d3575ab159948bfd2
thetime50 <1004763777@qq.com>
2022-08-24 22:44:35 +0800

    feat: controlManagerDemo

---
+++ create-app/src/pages/x6resize/controlManagerDemo.ts
@@ -0,0 +1,31 @@
+import {
+  testCreateGraphControlManager,
+  testGraphControlManagerExec,
+} from "./GraphControlManager";
+import HandleResizeControl from "./HandleResizeControl";
+import { Graph, Model, Node, Cell } from "@antv/x6";
+
+export function renderControlManager(dom: HTMLDivElement) {
+  const graph = new Graph({
+    container: dom,
+    mousewheel: true,
+    grid: true,
+    embedding: {
+      enabled: true,
+    },
+    highlighting: {
+      embedding: {
+        name: "stroke",
+        args: {
+          padding: -1,
+          attrs: {
+            stroke: "#73d13d",
+          },
+        },
+      },
+    },
+  });
+
+  const graphControlManager = testCreateGraphControlManager(graph);
+  testGraphControlManagerExec(graph, "add", new HandleResizeControl(graph));
+}
